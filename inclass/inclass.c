1e2d8c3e0c750dbd2c032e3733c1739247327e44
Yanggehan <33634849+Yanggehan@users.noreply.github.com>
2019-02-14 19:59:41 -0800

    11

--- inclass/inclass.c
+++ inclass/inclass.c
@@ -4,15 +4,13 @@
 
 
 
-//size allocated is 20 bytes. Regardless of which data you use. This way computer can change it at runtime
-
 union Data {
 
-   int i;  //4 bytes
+   int i;
 
-   float f; //4 bytes
+   float f;
 
-   char str[20];  //20 bytes
+   char str[20];
 
 };
 
@@ -21,15 +19,29 @@ union Data {
 int main( ) {
 
 
-
    union Data data;
 
 
-   printf( "Memory size occupied by data : %d\n", sizeof(data));
+
+   data.i = 10;
+
+   data.f = 220.5;  //writes over i
+
+   strcpy( data.str, "C Programming");  //writes over f
+
+
+
+   printf( "data.i : %d\n", data.i);    //print out garbage will read 4 bytes interpret as whole number
+
+   printf( "data.f : %f\n", data.f);    //print out garbage will read 4 bytes intrepret as
+
+   printf( "data.str : %s\n", data.str); //print out "C Programming"
 
 
    return 0;
 
 }
 
+
+//another example
 
