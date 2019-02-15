39ebace077a463e1adada5569617aafedf77e55c
Yanggehan <33634849+Yanggehan@users.noreply.github.com>
2019-02-14 19:56:56 -0800

    9

--- inclass/inclass.c
+++ inclass/inclass.c
@@ -17,12 +17,13 @@ struct Books {
 };
 
 
-
 /* function declaration */
 
-void printBook( struct Books *book );   //INPUT a pointer
+void printBook( struct Books book );
+
 
 int main( ) {
+
 
 
    struct Books Book1;        /* Declare Book1 of type Book */
@@ -41,7 +42,6 @@ int main( ) {
 
    Book1.book_id = 6495407;
 
-
    /* book 2 specification */
 
    strcpy( Book2.title, "Telecom Billing");
@@ -54,28 +54,29 @@ int main( ) {
 
 
 
-   /* print Book1 info by passing address of Book1 */
+   /* print Book1 info */
 
-   printBook(&Book1);
+   printBook( Book1 );
 
 
-   /* print Book2 info by passing address of Book2 */
+   /* Print Book2 info */
 
-   printBook( &Book2 );
+   printBook( Book2 );
 
    return 0;
 
 }
 
-void printBook( struct Books *book ) {
+ void printBook( struct Books book ) {
 
-   printf( "Book title : %s\n", book->title);
 
-   printf( "Book author : %s\n", book->author);
 
-   printf( "Book subject : %s\n", book->subject);
+	   printf( "Book title : %s\n", book.title);
 
-   printf( "Book book_id : %d\n", book->book_id);
+	   printf( "Book author : %s\n", book.author);
 
-}
-//Pointer to Struct
+	   printf( "Book subject : %s\n", book.subject);
+
+	   printf( "Book book_id : %d\n", book.book_id);
+
+	}
