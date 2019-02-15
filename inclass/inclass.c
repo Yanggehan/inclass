8ccac54ac73a5d0ccc76efad5015bcee115a394a
Yanggehan <33634849+Yanggehan@users.noreply.github.com>
2019-02-14 19:31:44 -0800

    7

--- inclass/inclass.c
+++ inclass/inclass.c
@@ -1,53 +1,79 @@
+
 #include <stdio.h>
 
-int main () {
-
-
-   int  var = 20;
-
-   int x = 10;
-
-   int  *ip;        /* pointer variable declaration */
+#include <string.h>
 
 
 
-   ip = NULL; // Point to address 0 for safety
+struct Books {
 
+   char  title[50];
 
-   ip = &var;  /* Change where you point to. Now pointing to address of var */
+   char  author[50];
 
+   char  subject[100];
 
+   int   book_id;
 
-   x = x + *ip; // x = 10 + 20. grabbing data where pointer is pointing is called dereferencing the pointer
-
-
-
-   ip = &x; //CHANGE WHERE ip points --> point to address of x
-
-
-
-   //showing pointer calculation
-
-   ip = ip + 3;  // address of ip + (3 * sizeof(int)) = address of ip + 12
-
-
-   printf("Address of var variable: %x\n", &var  );
+};
 
 
 
-   /* address stored in pointer variable */
-
-   printf("Address stored in ip variable: %x\n", ip );
+int main( ) {
 
 
-   /* access the value using the pointer */
 
-   printf("Value of *ip variable: %d\n", *ip );
+   struct Books Book1;        /* Declare Book1 of type Book */
+
+   struct Books Book2;        /* Declare Book2 of type Book */
+
+
+
+   /* book 1 specification */
+
+   strcpy( Book1.title, "C Programming");
+
+   strcpy( Book1.author, "Nuha Ali");
+
+   strcpy( Book1.subject, "C Programming Tutorial");
+
+   Book1.book_id = 6495407;
+
+
+   /* book 2 specification */
+
+   strcpy( Book2.title, "Telecom Billing");
+
+   strcpy( Book2.author, "Zara Ali");
+
+   strcpy( Book2.subject, "Telecom Billing Tutorial");
+
+   Book2.book_id = 6495700;
+
+
+
+   /* print Book1 info */
+
+   printf( "Book 1 title : %s\n", Book1.title);
+
+   printf( "Book 1 author : %s\n", Book1.author);
+
+   printf( "Book 1 subject : %s\n", Book1.subject);
+
+   printf( "Book 1 book_id : %d\n", Book1.book_id);
+
+   /* print Book2 info */
+
+   printf( "Book 2 title : %s\n", Book2.title);
+
+   printf( "Book 2 author : %s\n", Book2.author);
+
+   printf( "Book 2 subject : %s\n", Book2.subject);
+
+   printf( "Book 2 book_id : %d\n", Book2.book_id);
 
    return 0;
 
 }
 
-
-
-//STRUCT
+//Pointer to Struct
